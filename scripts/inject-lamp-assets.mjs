f592f89dad580a6bbf9a9ad5c56d4e458227b411
Tye1029 <tyetlt123@gmail.com>
2026-07-27 10:59:39 -0400

    Bump safe lamp runtime assets to version 12

--- scripts/inject-lamp-assets.mjs
+++ scripts/inject-lamp-assets.mjs
@@ -4,9 +4,9 @@ const indexUrl = new URL('../index.html', import.meta.url);
 const startMarker = '<!-- MODULAR_LAMP_ASSETS_START -->';
 const endMarker = '<!-- MODULAR_LAMP_ASSETS_END -->';
 const block = `${startMarker}\n` +
-  '  <script src="/assets/roulette/lamp-config.js?v=11" defer></script>\n' +
-  '  <script src="/assets/roulette/lamp.js?v=11" defer></script>\n' +
-  '  <script src="/assets/roulette/lamp-bootstrap.js?v=11" defer></script>\n' +
+  '  <script src="/assets/roulette/lamp-config.js?v=12" defer></script>\n' +
+  '  <script src="/assets/roulette/lamp.js?v=12" defer></script>\n' +
+  '  <script src="/assets/roulette/lamp-bootstrap.js?v=12" defer></script>\n' +
   `${endMarker}`;
 
 let html = await readFile(indexUrl, 'utf8');
