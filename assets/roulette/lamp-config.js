5db7d742d50f28ad7637c4b383bb499a1c0951fa
Tye1029 <tyetlt123@gmail.com>
2026-07-27 10:48:25 -0400

    Apply saved lamp calibration as permanent defaults

--- assets/roulette/lamp-config.js
+++ assets/roulette/lamp-config.js
@@ -6,39 +6,39 @@
 
   const groups = [
     ['Lamp PNG', [
-      ['lampArtX', 'PNG horizontal', [0, -35, 35, 0.25]],
-      ['lampWidth', 'PNG width', [56, 25, 100, 0.5]],
-      ['lampArtY', 'PNG vertical', [84.9, 55, 115, 0.25]],
-      ['lampScale', 'PNG scale', [1, 0.55, 1.8, 0.01]],
-      ['lampGlow', 'PNG glow', [0.82, 0, 1.5, 0.01]]
+      ['lampArtX', 'PNG horizontal', [-0.75, -35, 35, 0.25]],
+      ['lampWidth', 'PNG width', [94, 25, 100, 0.5]],
+      ['lampArtY', 'PNG vertical', [90.5, 55, 115, 0.25]],
+      ['lampScale', 'PNG scale', [1.1, 0.55, 1.8, 0.01]],
+      ['lampGlow', 'PNG glow', [0.8, 0, 1.5, 0.01]]
     ]],
     ['Rig and chains', [
-      ['lampX', 'Rig + chains X', [50, 25, 75, 0.25]],
-      ['lampY', 'Lamp rig vertical', [0, -120, 140, 1]],
-      ['chainHeight', 'Chain anchor height', [25, 5, 70, 0.5]],
-      ['chainLeftLength', 'Left chain length', [100, 20, 180, 1]],
-      ['chainRightLength', 'Right chain length', [100, 20, 180, 1]],
-      ['chainWidth', 'Chain width', [16, 2, 50, 0.5]],
-      ['chainStretch', 'Chain horizontal scale', [1, 0.35, 3, 0.01]]
+      ['lampX', 'Rig + chains X', [49.75, 25, 75, 0.25]],
+      ['lampY', 'Lamp rig vertical', [-26, -120, 140, 1]],
+      ['chainHeight', 'Chain anchor height', [5, 5, 70, 0.5]],
+      ['chainLeftLength', 'Left chain length', [70, 20, 180, 1]],
+      ['chainRightLength', 'Right chain length', [101, 20, 180, 1]],
+      ['chainWidth', 'Chain width', [12.5, 2, 50, 0.5]],
+      ['chainStretch', 'Chain horizontal scale', [0.56, 0.35, 3, 0.01]]
     ]],
     ['Swing', [
       ['swing', 'Swing amount', [1.35, 0, 10, 0.05]],
       ['speed', 'Swing duration (sec)', [5.6, 1.2, 14, 0.1]]
     ]],
     ['Light', [
-      ['lightHue', 'Light color / hue', [34, 0, 360, 1]],
-      ['lightSaturation', 'Light saturation', [88, 0, 100, 1]],
-      ['lightX', 'Light horizontal', [50, 10, 90, 0.5]],
-      ['lightY', 'Light vertical', [43, 10, 90, 0.5]],
-      ['spreadX', 'Light width', [58, 20, 120, 1]],
-      ['spreadY', 'Light depth', [64, 20, 140, 1]],
-      ['strength', 'Light strength', [0.55, 0.05, 1.25, 0.01]],
-      ['track', 'Tracking distance', [6, 0, 20, 0.25]],
+      ['lightHue', 'Light color / hue', [41, 0, 360, 1]],
+      ['lightSaturation', 'Light saturation', [100, 0, 100, 1]],
+      ['lightX', 'Light horizontal', [55.5, 10, 90, 0.5]],
+      ['lightY', 'Light vertical', [39, 10, 90, 0.5]],
+      ['spreadX', 'Light width', [75, 20, 120, 1]],
+      ['spreadY', 'Light depth', [80, 20, 140, 1]],
+      ['strength', 'Light strength', [0.25, 0.05, 1.25, 0.01]],
+      ['track', 'Tracking distance', [7.5, 0, 20, 0.25]],
       ['trackSpeed', 'Tracking duration (sec)', [5.6, 1.2, 14, 0.1]]
     ]],
     ['Room and gun', [
-      ['wallDark', 'Room darkness', [0.45, 0, 0.92, 0.01]],
-      ['gunGleam', 'Gun glint', [0.25, 0, 1.5, 0.01]]
+      ['wallDark', 'Room darkness', [0.92, 0, 0.92, 0.01]],
+      ['gunGleam', 'Gun glint', [0.03, 0, 1.5, 0.01]]
     ]]
   ];
 
