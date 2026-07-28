43f6e165207d22cd50da67ef9041baed8c1b6284
Tye1029 <tyetlt123@gmail.com>
2026-07-28 18:22:16 -0400

    Refresh scene settings and media assets

--- scripts/inject-lamp-assets.mjs
+++ scripts/inject-lamp-assets.mjs
@@ -59,7 +59,7 @@ function removeObsoleteLampBlocks(source) {
 }
 
 const criticalStyle = `  <link id="rrLampExternalStyles" rel="stylesheet" href="/assets/roulette/lamp.css?v=18&smoke=1">
-  <link id="rrAtmosphereSettingsStyles" rel="stylesheet" href="/assets/roulette/atmosphere-settings.css?v=1">
+  <link id="rrAtmosphereSettingsStyles" rel="stylesheet" href="/assets/roulette/atmosphere-settings.css?v=2">
   <style id="rrLampCriticalHide">
     [data-roulette-game] > .rr-lamp,
     [data-roulette-game] .rr-lamp-fixture,
@@ -94,9 +94,9 @@ const block = `${startMarker}\n` +
   `${criticalStyle}\n` +
   '  <script src="/assets/roulette/lamp-config.js?v=19" defer></script>\n' +
   '  <script src="/assets/roulette/lamp.js?v=20&smoke=1" defer></script>\n' +
-  '  <script src="/assets/roulette/lamp-bootstrap.js?v=19&settings=1" defer></script>\n' +
-  '  <script src="/assets/roulette/atmosphere-settings.js?v=1" defer></script>\n' +
-  '  <script src="/assets/roulette/audio-manager.js?v=4&ambience=2&countdown=2&audible=3&media=1" defer></script>\n' +
+  '  <script src="/assets/roulette/lamp-bootstrap.js?v=19&settings=2" defer></script>\n' +
+  '  <script src="/assets/roulette/atmosphere-settings.js?v=2" defer></script>\n' +
+  '  <script src="/assets/roulette/audio-manager.js?v=4&ambience=2&countdown=2&audible=3&media=2" defer></script>\n' +
   '  <script src="/assets/roulette/spin-audio-policy.js?v=3&turn-audio=2" defer></script>\n' +
   '  <script src="/assets/roulette/turn-animation.js?v=5" defer></script>\n' +
   '  <script src="/assets/roulette/turn-fire.js?v=2" defer></script>\n' +
